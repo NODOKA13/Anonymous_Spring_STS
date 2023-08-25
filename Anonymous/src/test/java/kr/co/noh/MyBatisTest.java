83dd07ad6fab3f16ad91b33c0887c54f0241323e
nonaka13 <nohjava@gmail.com>
2023-08-25 17:27:51 +0900

    기본설정 완료

--- Anonymous/src/test/java/kr/co/noh/MyBatisTest.java
+++ Anonymous/src/test/java/kr/co/noh/MyBatisTest.java
@@ -9,6 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//93
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 public class MyBatisTest {

2fa83ff87db47fdddfc262ab66e8e0d27c978f83
nonaka13 <nohjava@gmail.com>
2023-09-26 17:22:22 +0900

    페이징 완성

--- Anonymous/src/test/java/kr/co/noh/MySQLConnectionTest.java
+++ Anonymous/src/test/java/kr/co/noh/MySQLConnectionTest.java
@@ -1,27 +1,27 @@
-package kr.co.noh;
-
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-import org.junit.Test;
-
-//73
-public class MySQLConnectionTest {
-	
-	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private static final String URL = "jdbc:mysql://localhost:3306/anonymous?useSSL=false";
-	private static final String USER = "root";
-	private static final String PW = "12345";
-	
-	@Test
-	public void testConnection() throws Exception {
-		Class.forName(DRIVER);
-		
-		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
-			System.out.println(con);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-}
+//package kr.co.noh;
+//
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//
+//import org.junit.Test;
+//
+////73
+//public class MySQLConnectionTest {
+//	
+//	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+//	private static final String URL = "jdbc:mysql://localhost:3306/anonymous?useSSL=false";
+//	private static final String USER = "root";
+//	private static final String PW = "12345";
+//	
+//	@Test
+//	public void testConnection() throws Exception {
+//		Class.forName(DRIVER);
+//		
+//		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+//			System.out.println(con);
+//		}catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//}
